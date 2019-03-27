c52315ccee2d7efaabb45ca2b9c1937606d01974
hbj418 <hbj418@gmail.com>
2019-03-27 15:11:10 +0800

    testunit

--- location/tests/app/Http/Controllers/LocationControllerTest.php
+++ location/tests/app/Http/Controllers/LocationControllerTest.php
@@ -34,6 +34,28 @@ class LocationControllerTest extends TestCase
 
     public function testClosestSecrets()
     {
+        $currentLocation = [
+            'latitude' => 40.730610,
+            'longitude' => -73.935242
+        ];
+        $location = new App\Http\Controllers\LocationController();
+
+        $closestSecrets = $location->getClosestSecrets($currentLocation);
+        $this->assertClassHasStaticAttribute('conversionRates',
+            App\Http\Controllers\LocationController::class);
+        $this->assertContainsOnly('array', $closestSecrets);
+        $this->assertCount(3, $closestSecrets);
+        // Checking the first element
+        $currentElement = array_shift($closestSecrets);
+        $this->assertArraySubset(['name' => 'amber'], $currentElement);
+        // Second
+        $currentElement = array_shift($closestSecrets);
+        $this->assertArraySubset(['name' => 'ruby'], $currentElement);
+        // Third
+        $currentElement = array_shift($closestSecrets);
+        $this->assertArraySubset(['name' => 'diamond'], $currentElement);
 
     }
+
+
 }
